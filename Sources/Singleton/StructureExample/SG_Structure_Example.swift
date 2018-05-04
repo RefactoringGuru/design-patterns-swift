60d4adf034148a9be5df6fbcb66fe31d3f82530b
Maxim Eremenko <devmeremenko@gmail.com>
2018-05-04 14:31:34 +0300

    Singleton: Structure example was added

---
+++ Sources/Singleton/StructureExample/SG_Structure_Example.swift
@@ -0,0 +1,50 @@
+//
+//  SG_Structure_Example.swift
+//  PatternsForRefactoringGuru
+//
+//  Created by Maxim Eremenko on 4/30/18.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+class SG_Structure_Example_Tests: XCTestCase {
+    
+    func testSingletonSimple() {
+        
+        print(Singleton.shared.someBusinessLogic())
+    }
+}
+
+class Singleton {
+    
+    /**
+     * Static method, which controls access to singleton instance.
+     *
+     * This implementation allows creating singleton subclasses and having
+     * just one instance of each subclcass.
+     */
+    
+    static var shared: Singleton = {
+        let instance = Singleton()
+        
+        // configure
+        
+        return instance
+    }()
+    
+    /**
+     * Singleton initializer should always be private.
+     */
+    
+    private init() {}
+    
+    /**
+     * Finally, singleton has some business logic, which can be executed on
+     * its instance.
+     */
+    func someBusinessLogic() -> String {
+        // ...
+        return "Result of the 'someBusinessLogic' call"
+    }
+}
