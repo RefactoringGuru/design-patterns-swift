4b02476b995b9f3550389aa0658a84742bfbc57a
Maxim Eremenko <devmeremenko@gmail.com>
2018-05-08 17:38:30 +0300

    Prototype: NSCopying example was updated

---
+++ Sources/Prototype/NSCopying_Example/PT_NSCopying_ClientCode.swift
@@ -0,0 +1,34 @@
+//
+//  PT_NSCopying_ClientCode.swift
+//  Patterns.RefactoringGuru
+//
+//  Created by Maxim Eremenko on 4/28/18.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+/**
+ * Prototype Design Pattern
+ *
+ * Intent: Specify the kinds of objects to create using a prototypical instance,
+ * and create new objects by copying this prototype.
+ */
+
+class Prototype_SimpleExample: XCTestCase {
+    
+    /// In Swift, objects can be copied by adopting
+    /// NSCopying protocol or providing a custom implementation.
+    
+    func testPrototype_NSCopying() {
+        
+        let prototype = SubPrototype()
+        prototype.update(intValue: 2, stringValue: "Value2")
+        
+        guard let anotherPrototype = prototype.copy() as? SubPrototype else {
+            XCTAssert(false)
+            return
+        }
+        XCTAssert(anotherPrototype == prototype)
+    }
+}
