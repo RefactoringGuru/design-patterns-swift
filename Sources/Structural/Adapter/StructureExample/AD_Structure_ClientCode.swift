c2a6aec2efed1bb468ee151f413ebe9226800cb4
Maxim Eremenko <devmeremenko@gmail.com>
2018-05-23 19:35:30 +0300

    Project: Structure was updated

--- Sources/Structural/Adapter/StructureExample/AD_Structure_ClientCode.swift
+++ Sources/Structural/Adapter/StructureExample/AD_Structure_ClientCode.swift
@@ -44,6 +44,10 @@ private class Target {
 }
 
 private class Adaptee {
+    
+}
+
+extension Adaptee {
     /// The Adaptee contains some useful behavior, but its interface is incompatible
     /// with the existing client code. The Adaptee needs some adaptation before the
     /// client code can use it.
---
+++ Sources/Structural/Bridge/RealWorldExample/BR_Real_ClientCode.swift
@@ -0,0 +1,20 @@
+//
+//  BR_Real_ClientCode.swift
+//  RefactoringGuru.Patterns
+//
+//  Created by Maxim Eremenko on 5/23/18.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+class BR_Real_ClientCode: XCTestCase {
+    
+    func testBridgeReal() {
+        
+    }
+    
+    func clientCode() {
+        
+    }
+}
