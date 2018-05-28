f3f2e0490c839c03df4d4ef259d11fdea78849fd
Maxim Eremenko <devmeremenko@gmail.com>
2018-05-28 17:42:54 +0300

    Bridge: Real world example was added

--- Sources/Bridge/RealWorldExample/BR_Real_ClientCode.swift
+++ Sources/Bridge/RealWorldExample/BR_Real_ClientCode.swift
@@ -12,9 +12,101 @@ class BR_Real_ClientCode: XCTestCase {
     
     func testBridgeReal() {
         
+        let newsModel = NewsDomainModel(title: "Bridge example is available for Swift!",
+                                        images: [UIImage()])
+        
+        let appleModel = AppleDomainModel(calories: 47, photo: UIImage())
+        let appleAdapter = AppleSharingAdapter(appleModel,
+                                              title: "Yeah, I wanna share this Apple!")
+        
+        let instagram = InstagramSharingService()
+        let facebook = FaceBookSharingService()
+        
+        print("Sharing an Apple model to social networks")
+        /// share apple model
+        share(content: appleAdapter, using: instagram)
+        share(content: appleAdapter, using: facebook)
+        
+        print("Sharing a News model to social networks")
+        /// share news model
+        share(content: newsModel, using: instagram)
+        share(content: newsModel, using: facebook)
     }
     
-    func clientCode() {
+    func share(content: Content, using service: SharingService) {
+        service.share(content: content)
+    }
+}
+
+protocol SharingService {
+    
+    /// Abstraction
+    func share(content: Content)
+}
+
+protocol Content: CustomStringConvertible {
+    
+    /// Implementation
+    var title: String { get }
+    var images: [UIImage] { get }
+}
+
+/// Models
+struct NewsDomainModel: Content {
+    
+    var title: String
+    var images: [UIImage]
+    
+    var description: String {
+        return "News Model"
+    }
+}
+
+struct AppleDomainModel: CustomStringConvertible {
+    
+    var calories: Int
+    var photo: UIImage
+    
+    var description: String {
+        return "Apple Model"
+    }
+}
+
+struct AppleSharingAdapter: Content {
+    
+    private let food: AppleDomainModel
+    
+    var title: String
+    
+    var images: [UIImage] {
+        return [food.photo]
+    }
+    
+    init(_ food: AppleDomainModel, title: String = "") {
+        self.food = food
+        self.title = title
+    }
+    
+    var description: String {
+        return self.food.description
+    }
+}
+
+/// Services
+class FaceBookSharingService: SharingService {
+    
+    func share(content: Content) {
+        /// Use FaceBook API to share a content
         
+        print("\(content) was posted to the Facebook")
+    }
+}
+
+class InstagramSharingService: SharingService {
+    
+    func share(content: Content) {
+        /// Use Instagram API to share a content
+        
+        print("\(content) was posted to the Instagram")
     }
 }
