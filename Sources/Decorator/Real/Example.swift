38ad939ae85a5da08c3ba82ac145c4ebd74ea13d
Maxim Eremenko <devmeremenko@gmail.com>
2018-06-22 22:44:20 +0300

    Target for Decorator Real was created

--- Sources/Decorator/Real/Example.swift
+++ Sources/Decorator/Real/Example.swift
@@ -1,6 +1,6 @@
 //
-//  CompositeReal.swift
-//  CompositeReal
+//  DecoratorReal.swift
+//  DecoratorReal
 //
 //  Created by Maxim Eremenko on 6/22/18.
 //  Copyright © 2018 Eremenko Maxim. All rights reserved.
@@ -8,6 +8,6 @@
 
 import XCTest
 
-class CompositeRealExample: XCTestCase {
-    ///TODO:
+class DecoratorRealExample: XCTestCase {
+    
 }
