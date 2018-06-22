51ccbc30b7fa1b65f7006be87bb28b0ecea2d9f3
Maxim Eremenko <devmeremenko@gmail.com>
2018-06-22 22:40:45 +0300

    Target for Composite Real was created

---
+++ Sources/Composite/Real/Example.swift
@@ -0,0 +1,13 @@
+//
+//  CompositeReal.swift
+//  CompositeReal
+//
+//  Created by Maxim Eremenko on 6/22/18.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+class CompositeReal: XCTestCase {
+    ///TODO:
+}
