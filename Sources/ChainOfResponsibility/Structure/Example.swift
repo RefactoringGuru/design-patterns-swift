c40e7f242d8960a6b62012a4c930de1433bcfe62
Maxim Eremenko <devmeremenko@gmail.com>
2018-07-11 13:29:37 +0300

    Command, CoR, Iterator and Proxy examples were set up

---
+++ Sources/ChainOfResponsibility/Structure/Example.swift
@@ -0,0 +1,13 @@
+//
+//  ChainOfResponsibilityStructureExample.swift
+//  ChainOfResponsibilityStructure
+//
+//  Created by Maxim Eremenko on 7/11/18.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+class ChainOfResponsibilityStructureExample: XCTestCase {
+    
+}
