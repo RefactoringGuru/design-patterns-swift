3a2c3638fdb297449e52d5c259f8df7af538b721
Maxim Eremenko <devmeremenko@gmail.com>
2018-07-25 13:27:17 +0300

    Iterator Structure: iterators were separated

--- Sources/Iterator/Structure/Example.swift
+++ Sources/Iterator/Structure/Example.swift
@@ -13,53 +13,66 @@ import XCTest
 /// Intent: Provide a way to traverse the elements of an aggregate object without
 /// exposing its underlying representation.
 ///
+/// The client code may or may not know about the Concrete Iterator or
+/// Collection classes, depending on the level of indirection you want to keep in
+/// your program.
+///
+/// Example:
+/// Both IteratorProtocol and AnyIterator can be used to traverse a collection.
+/// Different collection-classes are used to separate iterators.
+///
 /// RU: Паттерн Итератор
 ///
 /// Назначение: Предоставляет возможность обходить элементы составного объекта,
 /// не раскрывая его внутреннего представления.
+///
+/// Клиентский код может знать или не знать о Конкретном Итераторе или
+/// классах Коллекций, в зависимости от уровня косвенности, который вы хотите
+/// сохранить в своей программе.
+///
+/// Пример:
+/// IteratorProtocol и AnyIterator могут быть использованы для обхода коллекции.
+/// Для разделение итераторов используются разные классы коллекции.
 
 class IteratorStructureExample: XCTestCase {
     
-    /// EN: There's also a built-in interface for collections:
+    /// EN: There's a built-in interface for collections:
     ///
     /// RU: Также есть встроенный интерфейс для коллекций:
     ///
-    /// https://developer.apple.com/documentation/swift/iteratorprotocol
+    /// IteratorProtocol: https://developer.apple.com/documentation/swift/iteratorprotocol
+    /// AnyIterator: https://developer.apple.com/documentation/swift/anyiterator
     
-    func test() {
+    func testIteratorProtocol() {
         
-        /// EN: The client code may or may not know about the Concrete Iterator or
-        /// Collection classes, depending on the level of indirection you want to keep in
-        /// your program.
-        ///
-        /// RU: Клиентский код может знать или не знать о Конкретном Итераторе или
-        /// классах Коллекций, в зависимости от уровня косвенности, который вы хотите
-        /// сохранить в своей программе.
+        let words = WordsCollection()
+        words.append("First")
+        words.append("Second")
+        words.append("Third")
         
-        let collection = WordsCollection()
-        collection.append("First")
-        collection.append("Second")
-        collection.append("Third")
+        print("Straight traversal using IteratorProtocol:")
+        clientCode(sequence: words)
+    }
+    
+    func testAnyIterator() {
         
-        print("Straight traversal:")
+        let numbers = NumbersCollection()
+        numbers.append(1)
+        numbers.append(2)
+        numbers.append(3)
         
-        for item in collection {
-            print(item)
-        }
+        print("\nReverse traversal using AnyIterator:")
+        clientCode(sequence: numbers)
+    }
+    
+    func clientCode<S: Sequence>(sequence: S) {
         
-        print("\nReverse traversal:")
-        
-        for item in collection.reverseIterator() {
+        /// Note: Client does not know the internal representation of a given sequence.
+        for item in sequence {
             print(item)
         }
     }
 }
-
-/// EN: Concrete Collections provide one or several methods for retrieving fresh
-/// iterator instances, compatible with the collection class.
-///
-/// RU: Конкретные Коллекции предоставляют один или несколько методов для
-/// получения новых экземпляров итератора, совместимых с классом коллекции.
 
 class WordsCollection {
     
@@ -70,29 +83,35 @@ class WordsCollection {
     }
 }
 
+class NumbersCollection {
+    
+    fileprivate lazy var items = [Int]()
+    
+    func append(_ item: Int) {
+        self.items.append(item)
+    }
+}
+
 extension WordsCollection: Sequence {
     
-    func makeIterator() -> CustomIterator {
-        return CustomIterator(self)
+    func makeIterator() -> WordsIterator {
+        return WordsIterator(self)
     }
+}
+
+extension NumbersCollection: Sequence {
     
-    /// EN: AnySequence and AnyIterator can be used to iterate a collection of any kind.
-    
-    func reverseIterator() -> AnySequence<String> {
-        
-        return AnySequence<String> { () -> AnyIterator<String> in
-            
-            var index = self.items.count - 1
-            
-            return AnyIterator {
-                defer { index -= 1 }
-                return index >= 0 ? self.items[index] : nil
-            }
+    func makeIterator() -> AnyIterator<Int> {
+        var index = self.items.count - 1
+
+        return AnyIterator {
+            defer { index -= 1 }
+            return index >= 0 ? self.items[index] : nil
         }
     }
 }
 
-class CustomIterator: IteratorProtocol {
+class WordsIterator: IteratorProtocol {
     
     /// EN: Concrete Iterators implement various traversal algorithms. These classes
     /// store the current traversal position at all times.
