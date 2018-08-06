fe414602450a3dbf153451571b53bfaa7403bef2
Maxim Eremenko <devmeremenko@gmail.com>
2018-08-06 23:54:15 +0300

    Observer Real: example was implemented

--- Sources/Observer/Real/Example.swift
+++ Sources/Observer/Real/Example.swift
@@ -10,4 +10,127 @@ import XCTest
 
 class ObserverReal: XCTestCase {
     
+    /// There are a number of ways to implement and use Observer pattern.
+    ///
+    /// KVO
+    /// Here is a great example of how to implement it in a dozen lines of code.
+    /// https://www.objc.io/blog/2018/04/24/bindings-with-kvo-and-keypaths/
+    ///
+    /// NotificationCenter
+    /// https://developer.apple.com/documentation/foundation/notificationcenter
+    ///
+    /// Rx
+    ///
+    /// And any other custom implementation of this pattern.
+    
+    func test() {
+        
+        let cartManager = CartManager()
+        
+        let navigationBar = UINavigationBar()
+        let cartVC = CartViewController()
+        
+        cartManager.add(subscriber: navigationBar)
+        cartManager.add(subscriber: cartVC)
+        
+        let apple = Food(id: 111, name: "Apple", price: 10, calories: 20)
+        cartManager.add(product: apple)
+        
+        let tShirt = Clothes(id: 222, name: "T-shirt", price: 200, size: "L")
+        cartManager.add(product: tShirt)
+        
+        cartManager.remove(product: apple)
+    }
+}
+
+protocol CartSubscriber: CustomStringConvertible {
+    
+    func accept(changed cart: [Product])
+}
+
+protocol Product {
+    
+    var id: Int { get }
+    var name: String { get }
+    var price: Double { get }
+    
+    func isEqual(to product: Product) -> Bool
+}
+
+extension Product {
+    
+    func isEqual(to product: Product) -> Bool {
+        return id == product.id
+    }
+}
+
+struct Food: Product {
+    
+    var id: Int
+    var name: String
+    var price: Double
+    
+    /// Food-specific properties
+    var calories: Int
+}
+
+struct Clothes: Product {
+    
+    var id: Int
+    var name: String
+    var price: Double
+    
+    /// Clothes-specific properties
+    var size: String
+}
+
+class CartManager {
+    
+    private lazy var cart = [Product]()
+    private lazy var subscribers = [CartSubscriber]()
+    
+    func add(subscriber: CartSubscriber) {
+        print("CartManager: I'am adding a new subscriber: \(subscriber.description)")
+        subscribers.append(subscriber)
+    }
+    
+    func add(product: Product) {
+        print("\nCartManager: I'am adding a new product: \(product.name)")
+        cart.append(product)
+        notifySubscribers()
+    }
+    
+    func remove(subscriber filter: (CartSubscriber) -> (Bool)) {
+        guard let index = subscribers.index(where: filter) else { return }
+        subscribers.remove(at: index)
+    }
+    
+    func remove(product: Product) {
+        guard let index = cart.index(where: { $0.isEqual(to: product) }) else { return }
+        print("\nCartManager: Product '\(product.name)' is removed from a cart")
+        cart.remove(at: index)
+        notifySubscribers()
+    }
+    
+    private func notifySubscribers() {
+        subscribers.forEach({ $0.accept(changed: cart) })
+    }
+}
+
+extension UINavigationBar: CartSubscriber {
+    
+    func accept(changed cart: [Product]) {
+        print("UINavigationBar: Updating an appearance of navigation items")
+    }
+    
+    open override var description: String { return "UINavigationBar" }
+}
+
+class CartViewController: UIViewController, CartSubscriber {
+    
+    func accept(changed cart: [Product]) {
+        print("CartViewController: Updating an appearance of a list view with products")
+    }
+    
+    open override var description: String { return "CartViewController" }
 }
