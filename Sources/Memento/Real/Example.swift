374e9c1f5023a09c78f3f3f220a9e4e123f8a5f9
Maxim Eremenko <devmeremenko@gmail.com>
2018-08-07 12:23:03 +0300

    Memento Real: example was implemented

--- Sources/Memento/Real/Example.swift
+++ Sources/Memento/Real/Example.swift
@@ -10,4 +10,92 @@ import XCTest
 
 class MementoReal: XCTestCase {
     
+    /// Note: UndoManager can be used as an alternative
+    
+    func test() {
+        
+        let textView = UITextView()
+        let careTaker = CareTaker(textView)
+        
+        textView.text = "First Change"
+        careTaker.save()
+        
+        textView.text = "Second Change"
+        careTaker.save()
+        
+        textView.text = (textView.text ?? "") + " & Third Change"
+        textView.textColor = .red
+        careTaker.save()
+        
+        print(careTaker)
+        
+        print("Client: Perform Undo operation 2 times\n")
+        careTaker.undo()
+        careTaker.undo()
+        
+        print(careTaker)
+    }
+}
+
+class CareTaker: CustomStringConvertible {
+    
+    private lazy var mementos = [Memento]()
+    private let textView: UITextView
+    
+    init(_ textView: UITextView) {
+        self.textView = textView
+    }
+    
+    func save() {
+        mementos.append(textView.memento)
+    }
+    
+    func undo() {
+        guard !mementos.isEmpty else { return }
+        textView.restore(with: mementos.removeLast())
+    }
+    
+    var description: String {
+        return mementos.reduce("", { $0 + $1.description })
+    }
+}
+
+protocol Memento: CustomStringConvertible {
+    
+    var text: String { get }
+    var date: Date { get }
+}
+
+struct TextViewMemento: Memento {
+    
+    let text: String
+    let date = Date()
+    
+    let textColor: UIColor?
+    let selectedRange: NSRange
+    
+    var description: String {
+        let time = Calendar.current.dateComponents([.hour, .minute, .second, .nanosecond],
+                                                   from: date)
+        let color = String(describing: textColor)
+        return "Text: \(text)\n" + "Date: \(time.description)\n"
+            + "Color: \(color)\n" + "Range: \(selectedRange)\n\n"
+    }
+}
+
+extension UITextView {
+    
+    var memento: Memento {
+        return TextViewMemento(text: text,
+                               textColor: textColor,
+                               selectedRange: selectedRange)
+    }
+    
+    func restore(with memento: Memento) {
+        guard let textViewMemento = memento as? TextViewMemento else { return }
+        
+        text = textViewMemento.text
+        textColor = textViewMemento.textColor
+        selectedRange = textViewMemento.selectedRange
+    }
 }
