b5145e4eaa079dda5ab3b9fc8c6352275ba9c0eb
Maxim Eremenko <devmeremenko@gmail.com>
2018-08-09 13:59:46 +0300

    Visitor Real: example was implemented

--- Sources/Visitor/Real/Example.swift
+++ Sources/Visitor/Real/Example.swift
@@ -8,9 +8,91 @@
 
 import XCTest
 
-class VisitorReal: XCTestCase {
+class VisitorRealExample: XCTestCase {
     
     func test() {
         
+        let notifications: [Notification] = [Email(), SMS(), PushNotification()]
+        
+        clientCode(handle: notifications, with: DefaultPolicy())
+        
+        clientCode(handle: notifications, with: NightPolicy())
     }
+    
+    func clientCode(handle notifications: [Notification], with policy: NotificationPolicy) {
+        print("\nClient: Using \(policy.description):")
+        
+        notifications.forEach { item in
+            if item.isNotificationOn(for: policy) {
+                print("\t" + item.description + " notification will be shown")
+            } else {
+                print("\t" + item.description + " notification will be silenced")
+            }
+        }
+    }
+}
+
+/// Policy
+
+protocol NotificationPolicy: CustomStringConvertible {
+    
+    var isEmailOn: Bool { get }
+    var isSMSOn: Bool { get }
+    var isPushOn: Bool { get }
+}
+
+class NightPolicy: NotificationPolicy {
+    
+    var isEmailOn: Bool { return false }
+    
+    var isSMSOn: Bool { return true }
+    
+    var isPushOn: Bool { return false }
+    
+    var description: String { return "Night Policy" }
+}
+
+class DefaultPolicy: NotificationPolicy {
+    
+    var isEmailOn: Bool { return true }
+    
+    var isSMSOn: Bool { return true }
+    
+    var isPushOn: Bool { return true }
+    
+    var description: String { return "Default Policy" }
+}
+
+/// Notifications
+
+protocol Notification: CustomStringConvertible {
+    
+    func isNotificationOn(for policy: NotificationPolicy) -> Bool
+}
+
+struct Email: Notification {
+    
+    func isNotificationOn(for policy: NotificationPolicy) -> Bool {
+        return policy.isEmailOn
+    }
+    
+    var description: String { return "Email" }
+}
+
+struct SMS: Notification {
+    
+    func isNotificationOn(for policy: NotificationPolicy) -> Bool {
+        return policy.isSMSOn
+    }
+    
+    var description: String { return "SMS" }
+}
+
+struct PushNotification: Notification {
+    
+    func isNotificationOn(for policy: NotificationPolicy) -> Bool {
+        return policy.isPushOn
+    }
+    
+    var description: String { return "Push" }
 }
