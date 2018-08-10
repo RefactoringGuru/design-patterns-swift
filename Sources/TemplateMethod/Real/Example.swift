23d3663346d31defaf8a3ed084c18b33bb90e053
Maxim Eremenko <devmeremenko@gmail.com>
2018-08-10 17:41:57 +0300

    Template Method Real: example was implemented

--- Sources/TemplateMethod/Real/Example.swift
+++ Sources/TemplateMethod/Real/Example.swift
@@ -7,7 +7,113 @@
 //
 
 import XCTest
+import AVFoundation
+import CoreLocation
+import Photos
 
 class TemplateMethodReal: XCTestCase {
     
+    /// A good example of Template Method is a life cycle of UIViewController
+    
+    func test() {
+        
+        let accessors = [CameraAccessor(), MicrophoneAccessor(), PhotoLibraryAccessor()]
+        
+        accessors.forEach { item in
+            item.requestAccessIfNeeded({ status in
+                let message = status ? "You have access to " : "You do not have access to "
+                print(message + item.description + "\n")
+            })
+        }
+    }
+}
+
+class PermissionAccessor: CustomStringConvertible {
+    
+    typealias Completion = (Bool) -> ()
+    
+    func requestAccessIfNeeded(_ completion: @escaping Completion) {
+        
+        guard !hasAccess() else { completion(true); return }
+        
+        willReceiveAccess()
+        
+        requestAccess { status in
+            status ? self.didReceiveAcesss() : self.didRejectAcesss()
+            
+            completion(status)
+        }
+    }
+    
+    func requestAccess(_ completion: @escaping Completion) {
+        fatalError("Should be overridden")
+    }
+    
+    func hasAccess() -> Bool {
+        fatalError("Should be overridden")
+    }
+    
+    var description: String { return "PermissionAccessor" }
+    
+    /// Hooks
+    func willReceiveAccess() {}
+    
+    func didReceiveAcesss() {}
+    
+    func didRejectAcesss() {}
+}
+
+class CameraAccessor: PermissionAccessor {
+    
+    override func requestAccess(_ completion: @escaping Completion) {
+        AVCaptureDevice.requestAccess(for: .video) { status in
+            return completion(status)
+        }
+    }
+    
+    override func hasAccess() -> Bool {
+        return AVCaptureDevice.authorizationStatus(for: .video) == .authorized
+    }
+    
+    override var description: String { return "Camera" }
+}
+
+class MicrophoneAccessor: PermissionAccessor {
+    
+    override func requestAccess(_ completion: @escaping Completion) {
+        AVAudioSession.sharedInstance().requestRecordPermission { status in
+            completion(status)
+        }
+    }
+    
+    override func hasAccess() -> Bool {
+        return AVAudioSession.sharedInstance().recordPermission() == .granted
+    }
+    
+    override var description: String { return "Microphone" }
+}
+
+class PhotoLibraryAccessor: PermissionAccessor {
+    
+    override func requestAccess(_ completion: @escaping Completion) {
+        PHPhotoLibrary.requestAuthorization { status in
+            completion(status == .authorized)
+        }
+    }
+    
+    override func hasAccess() -> Bool {
+        return PHPhotoLibrary.authorizationStatus() == .authorized
+    }
+    
+    override var description: String { return "PhotoLibrary" }
+    
+    override func didReceiveAcesss() {
+        /// We want to track how many people give access to the PhotoLibrary.
+        print("PhotoLibrary Accessor: Receive access. Updating analytics...")
+    }
+    
+    override func didRejectAcesss() {
+        /// ... and also we want to track how many people rejected access.
+        print("PhotoLibrary Accessor: Rejected with access. Updating analytics...")
+    }
 }
