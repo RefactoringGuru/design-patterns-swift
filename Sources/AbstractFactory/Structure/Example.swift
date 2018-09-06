6f0259a7be7dc44f8e4c23c04c14c5acf6815a1e
Alexander Shvets <neochief@shvetsgroup.com>
2018-09-06 13:31:07 +0300

    Updated comments.

--- Sources/AbstractFactory/Structure/Example.swift
+++ Sources/AbstractFactory/Structure/Example.swift
@@ -8,16 +8,25 @@
 
 import XCTest
 
-/// Abstract Factory Design Pattern
+/// EN: Abstract Factory Design Pattern
 ///
 /// Intent: Provide an interface for creating families of related or dependent
 /// objects without specifying their concrete classes.
+///
+/// RU: Паттерн Абстрактная Фабрика
+///
+/// Назначение: Предоставляет интерфейс для создания семейств связанных или
+/// зависимых объектов без привязки к их конкретным классам.
 
 class AbstractFactoryStructuralExample: XCTestCase {
     
-    /// The client code works with factories and products only through abstract
-    /// types: AbstractFactory and AbstractProducts. This let you pass any subtype of
-    /// the factory or product to the client code without breaking it.
+    /// EN: The client code works with factories and products only through abstract
+    /// types: AbstractFactory and AbstractProduct. This lets you pass any factory or
+    /// product subclass to the client code without breaking it.
+    ///
+    /// RU: Клиентский код работает с фабриками и продуктами только через абстрактные
+    /// типы: Абстрактная Фабрика и Абстрактный Продукт. Это позволяет передавать
+    /// любой подкласс фабрики или продукта клиентскому коду, не нарушая его.
     
     func testAbstractFactoryStructure() {
         
@@ -39,15 +48,20 @@ class AbstractFactoryStructuralExample: XCTestCase {
     }
 }
 
-/// Each distinct product of a product family should have a base interface. All
-/// variations of the product must implement this interface.
+/// EN: Each distinct product of a product family should have a base interface.
+/// All variations of the product must implement this interface.
+///
+/// RU: Каждый отдельный продукт семейства продуктов должен иметь базовый
+/// интерфейс. Все вариации продукта должны реализовывать этот интерфейс.
 
 protocol AbstractProductA {
     
     func usefulFunctionA() -> String
 }
 
-/// Concrete Products will be created by the corresponding concrete factory.
+/// EN: Concrete Products are created by corresponding Concrete Factories.
+///
+/// RU: Конкретные продукты создаются соответствующими Конкретными Фабриками.
 
 class ConcreteProductA1: AbstractProductA {
     
@@ -63,25 +77,38 @@ class ConcreteProductA2: AbstractProductA {
     }
 }
 
-/// The base interface of another product. Products can interact with each other,
-/// however a proper interaction is possible only between products of the same
-/// concrete variation.
+/// EN: The base interface of another product. All products can interact with
+/// each other, but proper interaction is possible only between products of the
+/// same concrete variation.
+///
+/// RU: Базовый интерфейс другого продукта. Все продукты могут взаимодействовать
+/// друг с другом, но правильное взаимодействие возможно только между продуктами
+/// одной и той же конкретной вариации.
 
 protocol AbstractProductB {
     
-    /// The ProductB is able to do its own thing...
+    /// EN: Product B is able to do its own thing...
+    ///
+    /// RU: Продукт B способен работать самостоятельно...
     
     func usefulFunctionB() -> String
     
-    /// ...but it also can collaborate with the ProductA.
+    /// EN: ...but it also can collaborate with the ProductA.
     ///
-    /// The Abstract Factory will make sure that the products it produces will be
-    /// of the same variation, which will make them compatible.
+    /// The Abstract Factory makes sure that all products it creates are of
+    /// the same variation and thus, compatible.
+    ///
+    /// RU: ...а также взаимодействовать с Продуктами Б той же вариации.
+    ///
+    /// Абстрактная Фабрика гарантирует, что все продукты, которые она
+    /// создает, имеют одинаковую вариацию и, следовательно, совместимы.
     
     func anotherUsefulFunctionB(collaborator: AbstractProductA) -> String
 }
 
-/// Concrete products will be created by the corresponding concrete factory.
+/// EN: Concrete Products are created by corresponding Concrete Factories.
+///
+/// RU: Конкретные Продукты создаются соответствующими Конкретными Фабриками.
 
 class ConcreteProductB1: AbstractProductB {
     
@@ -90,8 +117,13 @@ class ConcreteProductB1: AbstractProductB {
     }
     
     
-    /// The B1 product is only able to work correctly with the A1 product.
-    /// However, it still lists an abstract product A in its signature.
+    /// EN: This variant, Product B1, is only able to work correctly with the
+    /// variant, Product A1. Nevertheless, it accepts any instance of
+    /// AbstractProductA as an argument.
+    ///
+    /// RU: Продукт B1 может корректно работать только с Продуктом A1. Тем не
+    /// менее, он принимает любой экземпляр Абстрактного Продукта А в качестве
+    /// аргумента.
     
     func anotherUsefulFunctionB(collaborator: AbstractProductA) -> String {
         let result = collaborator.usefulFunctionA()
@@ -105,8 +137,13 @@ class ConcreteProductB2: AbstractProductB {
         return "The result of the B2 product."
     }
     
-    /// The B2 product is only able to work correctly with the A2 product.
-    /// However, it still lists an abstract product A in its signature.
+    /// EN: This variant, Product B2, is only able to work correctly with the
+    /// variant, Product A2. Nevertheless, it accepts any instance of
+    /// AbstractProductA as an argument.
+    ///
+    /// RU: Продукт B2 может корректно работать только с Продуктом A2. Тем не
+    /// менее, он принимает любой экземпляр Абстрактного Продукта А в качестве
+    /// аргумента.
     
     func anotherUsefulFunctionB(collaborator: AbstractProductA) -> String {
         let result = collaborator.usefulFunctionA()
@@ -114,42 +151,56 @@ class ConcreteProductB2: AbstractProductB {
     }
 }
 
-/// The Abstract Factory interface declares a set of methods that return
-/// different abstract products. These products are related and called a family.
-/// Products of one family are usually able to collaborate between themselves. A
-/// family of products may have several variations, but the products of one
-/// variation are incompatible with products of another.
+/// EN: The Abstract Factory interface declares a set of methods that return
+/// different abstract products. These products are called a family and are
+/// related by a high-level theme or concept. Products of one family are usually
+/// able to collaborate among themselves. A family of products may have several
+/// variations, but the products of one variation are incompatible with products
+/// of another.
+///
+/// RU: Интерфейс Абстрактной Фабрики объявляет набор методов, которые возвращают
+/// различные абстрактные продукты.  Эти продукты называются семейством и связаны
+/// темой или концепцией высокого уровня. Продукты одного семейства обычно могут
+/// взаимодействовать между собой. Семейство продуктов может иметь несколько
+/// вариаций,  но продукты одной вариации несовместимы с продуктами другой.
 
 protocol AbstractFactory {
-    
+
     func createProductA() -> AbstractProductA
     func createProductB() -> AbstractProductB
 }
 
-/// Concrete Factories produce a family of products that belong to a single
-/// variation. The factory guarantees that resulting products will be compatible.
+/// EN: Concrete Factories produce a family of products that belong to a single
+/// variation. The factory guarantees that resulting products are compatible.
 /// Note that signatures of the Concrete Factory's methods return an abstract
 /// product, while inside the method a concrete product is instantiated.
+///
+/// RU: Конкретная Фабрика производит семейство продуктов одной вариации. Фабрика
+/// гарантирует совместимость полученных продуктов.  Обратите внимание, что
+/// сигнатуры методов Конкретной Фабрики возвращают абстрактный продукт, в то
+/// время как внутри метода создается экземпляр  конкретного продукта.
 
 class ConcreteFactory1: AbstractFactory {
-    
+
     func createProductA() -> AbstractProductA {
         return ConcreteProductA1()
     }
-    
+
     func createProductB() -> AbstractProductB {
         return ConcreteProductB1()
     }
 }
 
-/// Each concrete factory has a corresponding product variation.
+/// EN: Each Concrete Factory has a corresponding product variation.
+///
+/// RU: Каждая Конкретная Фабрика имеет соответствующую вариацию продукта.
 
 class ConcreteFactory2: AbstractFactory {
-    
+
     func createProductA() -> AbstractProductA {
         return ConcreteProductA2()
     }
-    
+
     func createProductB() -> AbstractProductB {
         return ConcreteProductB2()
     }
